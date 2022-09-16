2d56feea68aceb92656dc051dd1dc2b1b087687c
agus_artero <agusartero7@gmail.com>
2022-09-15 23:45:46 -0300

    Principios de EVENTOS Y DOM

--- main.js
+++ main.js
@@ -1,13 +1,65 @@
 const productos = [
-  {nombre: "hamburguesa simple", precio: 600},
-  {nombre: "hamburguesa con cheddar", precio: 800},
-  {nombre: "hamburguesa con cheddar y bacon", precio: 1000},
-  {nombre: "papas fritas", precio: 400},
-  {nombre: "papas fritas con cheddar", precio: 600},
-  {nombre: "ensalada completa", precio: 800},
+  {
+    nombre: "hamburguesa simple",
+    precio: 600,
+    id: 1,
+    imagen: "./img/hamburguesa.png",
+  },
+  {
+    nombre: "hamburguesa con cheddar",
+    precio: 800,
+    id: 2,
+    imagen: "./img/hamburguesa2.jpg",
+  },
+  {
+    nombre: "hamburguesa con cheddar y bacon",
+    precio: 1000,
+    id: 3,
+    imagen: "./img/papas1.jpg",
+  },
+  {
+    nombre: "papas fritas",
+    precio: 400,
+    id: 4,
+    imagen: "./img/papas1.jpg",
+  },
+  {
+    nombre: "papas fritas con cheddar",
+    precio: 600,
+    id: 5,
+    imagen: "./img/papas2.jpg",
+  },
+  {
+    nombre: "ensalada completa",
+    precio: 800,
+    id: 6,
+    imagen: "./img/ensalada.jpg",
+  },
 ];
 
-let carrito = [];
+const createCards = () => {
+  let contenedor = document.getElementById("container");
+  productos.forEach((producto, posicion) => {
+    let card = document.createElement("div");
+    card.classList.add("card", "col-sm-12", "col-lg-3");
+    card.innerHTML = `
+    <img src="${producto.imagen}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${producto.nombre}</h5>
+    <p class="card-text">${producto.precio}</p>
+    <a href="#" class="btn btn-primary" onClick="addtoCarrito()">Añadir al carrito</a> </div>`;
+    contenedor.appendChild(card);
+  });
+};
+
+createCards();
+
+const addtoCarrito = () => {
+  alert("Acabas de agregar el producto al carrito");
+};
+
+// Proyecto anterior a DOM
+/*
 
 let seleccion = prompt("Hola! Desea ver el catalogo de la tienda?");
 
@@ -92,4 +144,4 @@ while (seleccion != "no") {
 const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
 console.log(`El total a pagar es: $${total}`);
 
-alert(`Precio a pagar: $${total}`);
+alert(`Precio a pagar: $${total}`); */
